91f9f866fd239c205b362b0c7669ebc1656c3e03
grybin <Gennadiy.Rybin@dataart.com>
2018-06-05 17:14:59 +0300

    +

--- src/reducers/visibilityFilter.js
+++ src/reducers/visibilityFilter.js
@@ -1,10 +1,10 @@
 export default function (state = 'SHOW_ALL', action) {
   console.log('*** from reducer:');
-  console.log(state, action);
+  console.log(action);
   switch (action.type) {
     case 'SET_VISIBILITY_FILTER':
       return action.filter;
     default:
-      return state
+      return state;
   }
 };
